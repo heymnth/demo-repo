a8ce25623f27924a01d9dd6fa99450cb54eef09e
heymnth <heymnthl8@gmail.com>
2025-10-18 15:07:52 +0530

    welcome

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Hello{
+    public static void main(String [] args){
+       System.out.println("welcome to java programming");
+
     }
 }
